a31fbb9918da894cd79920e941b3339afcb9a1a4
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2022-11-09 22:32:35 +0900

    fix 不要な記述削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import { Layout } from "../components/Layout";
+import { Layout } from '../components/Layout'
 
 export default function Home() {
   return (
-    <Layout title={"Home"}>
-      <p>hoge</p>
+    <Layout title={'Home'}>
+      <p>Home</p>
     </Layout>
-  );
+  )
 }
